bf01b4015783e13532030e0010b7a6ddcf4f802b
Alyssa <alyssahuang@college.harvard.edu>
2022-10-23 01:24:03 -0400

    Added debugging demo

---
+++ debugging_exercise.py
@@ -0,0 +1,12 @@
+def function1():
+    function2(4)
+    function2(5)
+    function2(6)
+    print("function1 done successfully")
+
+def function2(x):
+    if x % 5 == 0:
+        raise Exception("function2 failed")
+    return x + 2
+
+function1()
