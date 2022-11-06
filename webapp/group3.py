36535ad4a6490a395b064e0ae26481c9bd0e8766
blangman <blangman@college.harvard.edu>
2022-11-06 10:49:54 -0500

    Adding multiple routes

--- webapp/group3.py
+++ webapp/group3.py
@@ -9,8 +9,39 @@ from webapp.database import User
 
 bp = Blueprint('group3', __name__, url_prefix='/')
 
-@bp.route("success", methods=["GET", "POST"])
+@bp.route("group_profile", methods=["GET", "POST"])
 def sample():
     """render_pages"""
     # Change what template is being rendered
+    return render_template("group_profile.html")
+
+@bp.route("success", methods=["GET", "POST"])
+def success():
+    """render_pages"""
+    # Change what template is being rendered
     return render_template("success.html")
+
+@bp.route("user_profile", methods=["GET", "POST"])
+def user_profile():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("user_profile.html")
+
+@bp.route("group_matches", methods=["GET", "POST"])
+def group_matches():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("group_matches.html")
+
+@bp.route("contact", methods=["GET", "POST"])
+def contact():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("contact.html")
+
+@bp.route("index", methods=["GET", "POST"])
+def index():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("index.html")
+
