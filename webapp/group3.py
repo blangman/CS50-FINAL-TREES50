dc44ba24567a13913af5e7451f0cb2b5653708a7
blangman <blangman@college.harvard.edu>
2022-11-06 03:59:05 -0500

    final change

--- webapp/group3.py
+++ webapp/group3.py
@@ -9,39 +9,8 @@ from webapp.database import User
 
 bp = Blueprint('group3', __name__, url_prefix='/')
 
-@bp.route("index", methods=["GET", "POST"])
+@bp.route("contact", methods=["GET", "POST"])
 def sample():
     """render_pages"""
     # Change what template is being rendered
-    return render_template("index.html")
-
-@bp.route("contact", methods=["GET", "POST"])
-def contact():
-    """render_pages"""
-    # Change what template is being rendered
     return render_template("contact.html")
-
-@bp.route("group_matches", methods=["GET", "POST"])
-def matches():
-    """render_pages"""
-    # Change what template is being rendered
-    return render_template("group_matches.html")
-
-@bp.route("group_profile", methods=["GET", "POST"])
-def group_profile():
-    """render_pages"""
-    # Change what template is being rendered
-    return render_template("group_profile.html")
-
-@bp.route("success", methods=["GET", "POST"])
-def success():
-    """render_pages"""
-    # Change what template is being rendered
-    return render_template("success.html")
-
-@bp.route("user_profile", methods=["GET", "POST"])
-def user_profile():
-    """render_pages"""
-    # Change what template is being rendered
-    return render_template("user_profile.html")
-
