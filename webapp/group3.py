e8993338681e611f2fd5683d2128540a215daf26
blangman <blangman@college.harvard.edu>
2022-11-06 03:55:32 -0500

    committ djsbdfnkedw

--- webapp/group3.py
+++ webapp/group3.py
@@ -14,3 +14,34 @@ def sample():
     """render_pages"""
     # Change what template is being rendered
     return render_template("index.html")
+
+@bp.route("contact", methods=["GET", "POST"])
+def contact():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("contact.html")
+
+@bp.route("group_matches", methods=["GET", "POST"])
+def matches():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("group_matches.html")
+
+@bp.route("group_profile", methods=["GET", "POST"])
+def group_profile():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("group_profile.html")
+
+@bp.route("success", methods=["GET", "POST"])
+def success():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("success.html")
+
+@bp.route("user_profile", methods=["GET", "POST"])
+def user_profile():
+    """render_pages"""
+    # Change what template is being rendered
+    return render_template("user_profile.html")
+
