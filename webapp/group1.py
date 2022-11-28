3243066a56bb55b579d8565b53f06fd54b040e00
blangman <blangman@college.harvard.edu>
2022-11-27 21:00:52 -0500

    made changes

--- webapp/group1.py
+++ webapp/group1.py
@@ -13,23 +13,3 @@ bp = Blueprint('group1', __name__, url_prefix='/')
 def signup():
     '''Renders the signup page.'''
     return render_template("signup.html")
-
-@bp.route("profile", methods=["GET", "POST"])
-def profile():
-    '''Renders your profile.'''
-    return render_template("profile.html")
-
-@bp.route("login", methods=["GET", "POST"])
-def login():
-    '''Renders the login page.'''
-    return render_template("login.html")
-
-@bp.route("terms-of-service", methods=["GET", "POST"])
-def terms_of_service():
-    '''Renders the terms of service.'''
-    return render_template("terms-of-service.html")
-
-@bp.route("your-groups", methods=["GET", "POST"])
-def your_groups():
-    '''Renders your groups.'''
-    return render_template("your-groups.html")
