943be01219810ebeff76ec6c71750f380857c877
dantra7 <112649287+dantra7@users.noreply.github.com>
2022-11-29 19:29:12 +0000

    Rendering all the html pages

--- webapp/tree.py
+++ webapp/tree.py
@@ -12,3 +12,27 @@ bp = Blueprint('group1', __name__, url_prefix='/')
 @bp.route('/', methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+@bp.route('/lobed', methods=["GET", "POST"])
+def index():
+    return render_template("lobed.html")
+
+@bp.route('/pointed', methods=["GET", "POST"])
+def index():
+    return render_template("pointed.html")
+
+@bp.route('/misc', methods=["GET", "POST"])
+def index():
+    return render_template("misc.html")
+
+@bp.route('/needles', methods=["GET", "POST"])
+def index():
+    return render_template("needles.html")
+
+@bp.route('/string', methods=["GET", "POST"])
+def index():
+    return render_template("string.html")
+
+@bp.route('/teardrop', methods=["GET", "POST"])
+def index():
+    return render_template("teardrop.html")
