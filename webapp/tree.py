6920b900a8c301930a735ab92c8311946b966d48
blangman <blangman@college.harvard.edu>
2022-11-30 15:42:17 -0500

    kjerasbl

--- webapp/tree.py
+++ webapp/tree.py
@@ -4,7 +4,6 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from webapp.database import db_session
-from webapp.database import User
 
 bp = Blueprint('group1', __name__, url_prefix='/')
 
