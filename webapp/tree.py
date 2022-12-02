d70020def5bca2505142a8755c6046be3458dd1d
blangman <blangman@college.harvard.edu>
2022-12-01 23:28:14 -0500

    made small changes

--- webapp/tree.py
+++ webapp/tree.py
@@ -4,6 +4,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from webapp.database import db_session
+from webapp.database import Tree
 
 bp = Blueprint('group1', __name__, url_prefix='/')
 
