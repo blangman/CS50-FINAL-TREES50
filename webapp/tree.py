b5c795ede39f9816fbba3dc6b778097b4a75a2cc
dantra7 <112649287+dantra7@users.noreply.github.com>
2022-12-06 22:28:27 +0000

    tree comments

--- webapp/tree.py
+++ webapp/tree.py
@@ -1,17 +1,17 @@
-import functools
-
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from webapp.database import db_session
-from webapp.database import Tree
 import sqlite3
 
+# Import functions that we will use from flask in the webapp. 
+from flask import (
+    Blueprint, flash, redirect, render_template, request
+)
+
+# connect the database that was made in database.py to tree.py so it can be accessed.
 db = sqlite3.connect("tree.db", check_same_thread=False).cursor()
 
-
+# Be able to create routes with bp.
 bp = Blueprint('group1', __name__, url_prefix='/')
 
+# Go through and make routes for each page of the html and render the html template for those pages.
 @bp.route('/', methods=["GET", "POST"])
 def index():
     return render_template("index.html")
@@ -42,40 +42,44 @@ def teardrop():
 
 @bp.route('/finder', methods=["GET", "POST"])
 def finder():
+    # When the submit button is clicked,
     if request.method == "POST":
+        # access the information that the user submitted through the form
         leaftype = request.form.get("leaftype")
         barktype = request.form.get("barktype")
         fruittype = request.form.get("fruittype")
 
+        # If they failed to submit something, flash method at top of screen
         if not leaftype or not barktype or not fruittype:
             flash("*Missing Required Fields*")
 
+        # If they did submit something for each category, access the database to get the tree. 
         else: 
+            # Get trees from the database where all the criteria are true
             tree_name = db.execute("SELECT tree_name FROM Tree WHERE leaftype=:leaftype AND barktype=:barktype AND fruittype=:fruittype", {'leaftype': leaftype, 'barktype': barktype, 'fruittype': fruittype})
+            # turn that into a list to make it easier to manipulate and sort through. 
             tree = [tree[0] for tree in tree_name.fetchall()]
-            print(tree_name)
-
+            
+            # access all of the images that match the search criteria and once again put them as lists. 
             url = db.execute("SELECT image FROM Tree WHERE leaftype=:leaftype AND barktype=:barktype AND fruittype=:fruittype", {'leaftype': leaftype, 'barktype': barktype, 'fruittype': fruittype})
             image = [image[0] for image in url.fetchall()]
-            print(image)
-
+            
+            # The same as the above db.executes but with the html for the trees that match the criteria. 
             html = db.execute("SELECT html FROM Tree WHERE leaftype=:leaftype AND barktype=:barktype AND fruittype=:fruittype", {'leaftype': leaftype, 'barktype': barktype, 'fruittype': fruittype})
             file = [file[0] for file in html.fetchall()]
-            print(file)
-
+            
+            # if there are no trees that match the criteria, return not_found
             if len(tree) == 0:
                 return redirect("not_found")
+            # if trees do match the criteria, pass their information on to found.html 
             else:
                 return render_template("found.html", tree_name = tree, url = image, html = file)
-            
-            # final_result = [i[0] for i in cursor.fetchall()]
-
+    
+    # for when the method is get instead of post 
     return render_template("finder.html")
 
 @bp.route('/found', methods=["GET", "POST"])
 def found():
-    if request.method == "POST":
-        return render_template("/lobed")
     return render_template("found.html")
 
 @bp.route('/not_found', methods=["GET", "POST"])
